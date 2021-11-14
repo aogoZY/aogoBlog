53d2aa373c94739a1c2d06c142841d6e764dfdbf
aogoo <zhouyang@zhouyangdeMacBook-Pro.local>
2021-11-14 13:05:52 +0800

    update .gitignore

--- src/main/java/com/star/service/serviceTest.java
+++ src/main/java/com/star/service/serviceTest.java
@@ -20,11 +20,6 @@ import java.io.InputStream;
 import java.nio.file.FileStore;
 import java.util.List;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringBootTest(
-        webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT
-)
-@DirtiesContext
 public class serviceTest {
 
     @Autowired
@@ -34,27 +29,6 @@ public class serviceTest {
     private SqlSession sqlSession;
     private UserDao userDao;
 
-
-    @Before
-    public void init() throws IOException {
-//        1、读取配置文件
-        in = Resources.getResourceAsStream("application-dev.yml");
-//        2、创建sqlSessionFactory的构建对象
-        SqlSessionFactoryBuilder factoryBuilder = new SqlSessionFactoryBuilder();
-//        3、使用构造者创建工厂对象 sqlSessionFactory
-        SqlSessionFactory factory = factoryBuilder.build(in);
-//        4、使用selSessionFactory生产 sqlSession 对象
-        sqlSession = factory.openSession(true);
-//        5、使用sqlSession创建dao接口的代理对象
-        userDao = sqlSession.getMapper(UserDao.class);
-
-    }
-
-    @After
-    public void destroy() throws Exception {
-        sqlSession.close();
-        in.close();
-    }
 
 
     @Test
